d8b7f78df7c05bf8daa7eb72a6e3a3d00a9ee87f
esrasumer <esrasumer28@gmail.com>
2022-07-07 10:34:25 +0300

    first commit

---
+++ callback.js
@@ -0,0 +1,29 @@
+// *** belirtilen süre sonra console çalişir
+
+// setTimeout(() => {
+//     console.log("hello");
+// }, 5000); //terminalde "hello" 2sn sonra çıkar
+
+// *** belirtilen süre içinde sürekli çalışır
+
+// setInterval(() => {
+//     console.log("merhaba ben her saniye çalışırım");
+// }, 1000);
+
+// *** terminalde meerhab yazar
+
+// const sayHi = () => {
+//     console.log("merhaba");
+// };
+
+// sayHi();
+
+// *** sayHi ' ı başka bir parametrede çalıştıdık
+
+// const sayHi = (cb)=> {
+//     cb();
+// };
+
+// sayHi(()=>{
+//     console.log("Hello!");
+// });
